5b7470b015acbd0b4032771e2f1fd8f7ca232005
mahen <mahen518@gmail.com>
2023-07-14 11:59:56 +0530

    gateway-service

---
+++ gateway-service/src/main/java/com/training/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,24 @@
+package com.training.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator configureRoute(RouteLocatorBuilder builder) {
+		return builder.routes()
+						.route("userId", r->r.path("/home/**").uri("lb://USER-SERVICE"))
+						.route("courseID", r->r.path("/courseapi/**").uri("lb://COURSE-SERVICE"))
+						.build();
+	}
+
+}
---
+++ gateway-service/src/test/java/com/training/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
