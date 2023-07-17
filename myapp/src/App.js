78d2a86c77f23ae272d09c43dcbfdf48311f90d7
mahen <mahen518@gmail.com>
2023-07-17 10:41:32 +0530

    react code

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my React App!</h1>
+    </div>
+  );
+}
+
+export default App;
