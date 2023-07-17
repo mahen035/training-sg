c594c358ca32704523d807d87c7f4d94528e3233
mahen <mahen518@gmail.com>
2023-07-17 18:00:55 +0530

    fetch data

--- myapp/src/components/api/FetchData.js
+++ myapp/src/components/api/FetchData.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function FetchData(props) {
+
+    const myStyle = {
+        backgroundColor: 'green',
+        color:'white',
+        padding:'10px',
+        
+    }
+
+    const[data, setData] = useState([])
 
     const getData = function(){
         fetch('http://localhost:8484/home/users')
         .then(response=>response.json())
-        .then(data=>console.log(data))
+        .then(data=>setData(data))
     }
     return (
         <div>
             <button onClick={getData}>Get Data from API</button>
+            <table border="1" style={myStyle}>
+                <thead>
+                    <tr>
+                        <th>FIRST NAME</th>
+                        <th>LAST NAME</th>
+                        <th>AGE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.length > 0
+                        ?
+                        data.map((item) =>{
+                            return(
+                                <tr key={item.firstName}>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.age}</td>
+                                </tr>
+                            )
+                        })
+                        :
+                        'Click to fetch Data'
+                    }
+                </tbody>
+            </table>
+
         </div>
     );
 }
