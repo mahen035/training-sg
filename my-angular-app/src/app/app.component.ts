7f8e07626c8c051c406a267c76074dc6f01b3f3e
mahen <mahen518@gmail.com>
2023-07-18 10:24:48 +0530

    angular-app

---
+++ my-angular-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//Decorator
+@Component({   // Angular Component
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-angular-app';
+}
